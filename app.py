8343d4dc051fb90a51f6653e9b829e0800a0c0b0
Steve <tuemail@example.com>
2026-05-15 15:31:01 -0400

    primer commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+@app.route("/")
+def inicio ():
+    return render_template ("index.html")
+
+@app.route ("/jugadores")
+def jugadores ():
+    return render_template ("index.html")
+
+@app.route ("/calendario")
+def calendario ():
+    return render_template ("/index.html")
+
+@app.route ("/inscripcion")
+def inscripcion ():
+    return render_template ("/index,html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
